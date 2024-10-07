08402fa597f59ec8f83e3a671ab93d84a81e13c3
WisdomerLee <50577439+WisdomerLee@users.noreply.github.com>
2024-10-07 09:55:54 +0900

    재정 분석 agent 작성해보기
     csv파일에 저장된 재무관련 정보를 바탕으로 경쟁사 정보를 tavily search를 통해 얻은 뒤, 해당 내용을 바탕으로 문서를 작성하고, 문서가 올바르게 작성되었는지 검토하는 과정을 거치고, 검토를 통과하면 문서를 돌려주는 간단한 예시

--- AIAgent_Advanced/Fulll_Agent/finance_agent.py
+++ AIAgent_Advanced/Fulll_Agent/finance_agent.py
@@ -1,1 +1,138 @@
 # 프로젝트 폴더 내부에 간단한 csv 파일로 재무재표 데이터가 있다고 가정하고 프로젝트를 진행
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import json
+
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated, List
+import operator
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+import pandas as pd
+from io import StringIO
+from tavily import TavilyClient
+from pydantic import BaseModel # 강의에서는 langchain_core.pydantic_v1을 사용하나, langchain이 업데이트 되며 pydantic자체를 활용할 수 있게 바뀌고 있음
+
+# agent의 상태를 기억하기 위해 아래와 같이 먼저 선언
+memory = SqliteSaver.from_conn_string(":memory:")
+
+load_dotenv()
+
+openai_key = os.getenv("OPENAI_API_KEY")
+
+tavily = os.getenv("TAVILY_API_KEY")
+
+llm_name = "gpt-4o-mini"
+model = ChatOpenAI(api_key=openai_key, model=llm_name)
+
+tavily = TavilyClient(api_key=tavily)
+
+# LangGraph에서 공통으로 사용하게 될 class, 해당 클래스는 langgraph의 로직마다 모두 다 다르게 적용될 수 있음
+class AgentState(TypedDict):
+  task: str
+  competitors: List[str]
+  csv_file: str
+  financial_data: str
+  analysis: str
+  competitor_data: str
+  comparison: str
+  feedback: str
+  report: str
+  content: List[str]
+  revision_number: int
+  max_revision: int
+
+# 사용자의 요청을 모두 저장할 타입
+class Queries(BaseModel):
+  queries: List[str]
+
+# 노드 등의 agent들에서 사용하게 될 prompt들 < langsmith에 들어가서 그곳에서 본인에게 맞는 프롬프트를 찾아서 적용하거나 해당 프롬프트를 수정하여 아래에 각각 적용할 것
+GATHER_FINANCIALS_PROMPT = """"""
+ANALYZE_DATA_PROMPT = """"""
+RESEARCH_COMPETITORS_PROMPT = """"""
+COMPETE_PERFORMANCE_PROMPT = """"""
+
+FEEDBACK_PROMPT = """"""
+WRITE_REPORT_PROMPT = """"""
+RESEARCH_CRITIQUE_PROMPT = """"""
+
+# 노드에서 돌려주는 것들은 모두 langgraph로 빌드될 state의 이름, 데이터 타입이 모두 일치하는 형태로 있어야 함
+
+def gather_financials_node(state: AgentState):
+  csv_file = state.get("csv_file")
+  df = pd.read_csv(StringIO(csv_file))
+
+  financial_data_str = df.to_string(index=False)
+
+  combined_content = (
+    f"{state.get("task")}\n\n Here is the financial data:\n\n{financial_data_str}"
+  )
+  messages = [
+    SystemMessage(content=GATHER_FINANCIALS_PROMPT),
+    HumanMessage(content=combined_content),
+  ]
+  response = model.invoke(messages)
+  return {"financial_data": response.content}
+
+def analyze_data_node(state: AgentState):
+  messages = [
+    SystemMessage(content=ANALYZE_DATA_PROMPT),
+    HumanMessage(content=state.get("financial_data")),
+  ]
+  response = model.invoke(messages)
+  return {"analysis": response.content}
+
+def research_competitors_node(state: AgentState):
+  content = state.get("content", [])
+  for competitor in state.get("competitors"):
+    queries = model.with_structured_output(Queries).invoke(
+      [
+        SystemMessage(content=RESEARCH_COMPETITORS_PROMPT),
+        HumanMessage(content=competitor),
+      ]
+    )
+    for q in queries.queries:
+      response = tavily.search(query=q, max_results=2)
+      for r in response.get("results"):
+        content.append(r.get("content"))
+  return {"content": content}
+
+def compare_performance_node(state: AgentState):
+  content = "\n\n".join(state.get("content", []))
+  user_message = HumanMessage(
+    content=f"{state.get('task')}\n\nHere is the financial analysis {state.get('analysis')}"
+  )
+  messages = [
+    SystemMessage(content=COMPETE_PERFORMANCE_PROMPT.format(content=content)),
+    user_message,
+  ]
+  response = model.invoke(messages)
+  return {
+    "comparison": response.content,
+    "revision_number": state.get("revision_number", 1) + 1
+  }
+
+def research_critique_node(state: AgentState):
+  queries = model.with_structured_output(Queries).invoke(
+    [
+      SystemMessage(content=RESEARCH_CRITIQUE_PROMPT),
+      HumanMessage(content=state.get("feedback")),
+    ]
+  )
+  content = state.get("content", [])
+  for q in queries.queries:
+    response = tavily.search(query=q, max_results=2)
+    for r in response.get("results"):
+      content.append(r.get("content"))
+  return {"content": content}
+
+def write_report_node(state: AgentState):
+  messages = [
+    SystemMessage(content=WRITE_REPORT_PROMPT),
+    HumanMessage(content=state.get("comparison")),
+  ]
+  response = model.invoke(messages)
+  return {"report": response.content}
